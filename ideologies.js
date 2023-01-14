dd72389bee63c00405121497e6ee7f1f77a183fe
ebisuzawa <122659020+ebisuzawa@users.noreply.github.com>
2023-01-14 17:46:55 +0900

    Add files via upload

---
+++ ideologies.js
@@ -0,0 +1,90 @@
+ideologies = [
+    {
+        "name": "급진적 개혁주의",
+        "stats": {
+            "life": 90,
+            "womn": 90,
+            "bish": 90
+        }
+    },
+    {
+        "name": "개혁주의",
+        "stats": {
+            "life": 75,
+            "womn": 75,
+            "bish": 75
+        }
+    },
+    {
+        "name": "중도~상대적 개혁주의",
+        "stats": {
+            "life": 60,
+            "womn": 60,
+            "bish": 70
+        }
+    },
+    {
+        "name": "교회일치적 개혁주의",
+        "stats": {
+            "life": 65,
+            "womn": 65,
+            "bish": 50
+        }
+    },
+    {
+        "name": "교회일치적 보수주의",
+        "stats": {
+            "life": 35,
+            "womn": 50,
+            "bish": 35
+        }
+    },
+    {
+        "name": "보수주의",
+        "stats": {
+            "life": 30,
+            "womn": 35,
+            "bish": 35
+        }
+    },
+    {
+        "name": "중도~상대적 보수주의",
+        "stats": {
+            "life": 35,
+            "womn": 30,
+            "bish": 40
+        }
+    },
+    {
+        "name": "반동적 보수주의",
+        "stats": {
+            "life": 10,
+            "womn": 10,
+            "bish": 25
+        }
+    },
+    {
+        "name": "성직자 관료주의",
+        "stats": {
+            "life": 50,
+            "womn": 50,
+            "bish": 15
+        }
+    },
+    {
+        "name": "교회민주주의",
+        "stats": {
+            "life": 50,
+            "womn": 50,
+            "bish": 80
+        }
+    },
+    {
+        "name": "중도",
+        "stats": {
+            "life": 50,
+            "womn": 50,
+            "bish": 40
+        }
+    }
+];
