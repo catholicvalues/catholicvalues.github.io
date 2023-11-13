9544f65d1075b5f46df6ae9c59cb8a092b617b36
wereno4 <hellnow6312@gmail.com>
2023-11-13 14:07:06 +0900

    back with new questions

--- questions.js
+++ questions.js
@@ -1,14 +1,254 @@
-questions = [
+tmp_questions = [
   {
-    question: "example question",
+    question: "복음 실천이 목적임을 전제로, 사회 문제에 관심을 가질 필요가 있다.",
     effect: {
-      life: 0,
-      bish: 0,
-      womn: 0,
-      poli: 0,
+      comm: 0,
+      fath: 0,
+      socl: 5,
       mass: 0,
-      clth: 0,
-      host: 0,
+    }
+  },
+  {
+    question: "주교 혹은 사제들이 교회의 가르침에 바탕을 두고 사회적 불의에 대항하여 적극적인 행동을 취해야한다.",
+    effect: {
+      comm: 0,
+      fath: 0,
+      socl: 7.5,
+      mass: 0,
+    }
+  },
+  {
+    question: "사회 개혁에 참여하기보다 선행을 한 번 더 하는 것이 낫다.",
+    effect: {
+      comm: 0,
+      fath: 0,
+      socl: -5,
+      mass: 0,
+    }
+  },
+  {
+    question: "입으로 성체를 받아 모시는 것을 선호한다.",
+    effect: {
+      comm: 0,
+      fath: 0,
+      socl: 0,
+      mass: -7.5,
+    }
+  },
+  {
+    question: "성체와 성혈을 모두 영하는 양형 영성체가 자주 이루어져야한다.",
+    effect: {
+      comm: 0,
+      fath: 0,
+      socl: 0,
+      mass: -2.5,
+    }
+  },
+  {
+    question: "할 수 있다면 미사 중 오르간이 아닌 악기를 차용할 수 있다.",
+    effect: {
+      comm: 0,
+      fath: 0,
+      socl: 0,
+      mass: 7.5,
+    }
+  },
+  {
+    question: "본당의 단체 활동에 적극적으로 참여해야한다.",
+    effect: {
+      comm: 10,
+      fath: 0,
+      socl: 0,
+      mass: 0,
+    }
+  },
+  {
+    question: "기도, 묵상, 성경 봉독 등 신심 행위를 홀로 하는 것을 선호한다.",
+    effect: {
+      comm: -10,
+      fath: 0,
+      socl: 0,
+      mass: 0,
+    }
+  },
+  {
+    question: "신자들이 봉사활동, 기부 등을 적극적으로 행해야만 한다.",
+    effect: {
+      comm: 0,
+      fath: 2.5,
+      socl: -2.5,
+      mass: 0,
+    }
+  },
+  {
+    question: "사제가 제대를 향해 (회중을 등지고) 미사를 봉헌하는 것이 더 좋다.",
+    effect: {
+      comm: 0,
+      fath: 0,
+      socl: 0,
+      mass: -10,
+    }
+  },
+  {
+    question: "사회적 현안에 목소리를 내기 전에 자신이 신앙에 따라 올바르게 사는지 돌아볼 필요가 있다.",
+    effect: {
+      comm: 0,
+      fath: 0,
+      socl: 0,
+      mass: 0,
+    }
+  },
+  {
+    question: "성물을 많이 모으는 편이다.",
+    effect: {
+      comm: 0,
+      fath: 1.25,
+      socl: 0,
+      mass: 0,
+    }
+  },
+  {
+    question: "가톨릭 관련 출판물, 혹은 매체를 자주 챙겨본다.",
+    effect: {
+      comm: 0,
+      fath: 1.25,
+      socl: 0,
+      mass: 0,
+    }
+  },
+  {
+    question: "교회의 가르침에 맞는 정당 혹은 정치 세력을 강력히 지지해야한다.",
+    effect: {
+      comm: 0,
+      fath: 0,
+      socl: 12.5,
+      mass: 0,
+    }
+  },
+  {
+    question: "되도록 본당의 많은 교우들과 친교를 맺어야한다.",
+    effect: {
+      comm: 5,
+      fath: 0,
+      socl: 0,
+      mass: 0,
+    }
+  },
+  {
+    question: "사소한 죄라도 빠른 시일 내에 고해를 하는 편이다.",
+    effect: {
+      comm: 0,
+      fath: 10,
+      socl: 0,
+      mass: 0,
+    }
+  },
+  {
+    question: "신앙을 함께 할 친구가 있어야한다.",
+    effect: {
+      comm: 2.5,
+      fath: 0,
+      socl: 0,
+      mass: 0,
+    }
+  },
+  {
+    question: "솔직히 개인적으로는 받아들이기 힘든 교리가 있다.",
+    effect: {
+      comm: 0,
+      fath: -5,
+      socl: 0,
+      mass: 0,
+    }
+  },
+  {
+    question: "가까운 교우가 죄를 짓지 않도록 적극 응원해야 한다.",
+    effect: {
+      comm: 2.5,
+      fath: 0,
+      socl: -2.5,
+      mass: 0,
+    }
+  },
+  {
+    question: "내가 죄를 짓지 않도록 적극 응원해줄 교우가 필요하다.",
+    effect: {
+      comm: 2.5,
+      fath: 5,
+      socl: 0,
+      mass: 0,
+    }
+  },
+  {
+    question: "지킬 수 없는 상황이라도 신자로서의 의무를 무조건 지켜야한다.",
+    effect: {
+      comm: 0,
+      fath: 7.5,
+      socl: 0,
+      mass: 0,
+    }
+  },
+  {
+    question: "가기 어려운 환경이라도 가톨릭 성당을 찾아가 미사를 봉헌해야 한다.",
+    effect: {
+      comm: 0,
+      fath: 10,
+      socl: 0,
+      mass: 0,
+    }
+  },
+  {
+    question: "피정을 남들보다 자주하는 편이다.",
+    effect: {
+      comm: 0,
+      fath: 2.5,
+      socl: 0,
+      mass: 0,
+    }
+  },
+  {
+    question: "세속 법률 중 교회법·자연법과 합치하지 않는 법률이 있다면 전국민적 행동을 통해 시정해야한다.",
+    effect: {
+      comm: 0,
+      fath: 0,
+      socl: 7.5,
+      mass: 0,
+    }
+  },
+  {
+    question: "전례에 사회적 갈등 혹은 참사에 대한 기도지향을 넣는 것이 바람직하다.",
+    effect: {
+      comm: 0,
+      fath: 0,
+      socl: 2.5,
+      mass: 0,
+    }
+  },
+  {
+    question: "사제가 전례 경문을 재량껏 변형할 수 있다.",
+    effect: {
+      comm: 0,
+      fath: 0,
+      socl: 0,
+      mass: 5,
+    }
+  },
+  {
+    question: "장궤를 적극적으로 장려해야한다.",
+    effect: {
+      comm: 0,
+      fath: 0,
+      socl: 0,
+      mass: -5,
+    }
+  },
+  {
+    question: "미사 중 분향을 자주 행해야한다.",
+    effect: {
+      comm: 0,
+      fath: 0,
+      socl: 0,
+      mass: -7.5,
     }
   }
 ];
